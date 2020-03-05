b2f39786fa4a21bebbed2c90476bbc50c2bf1151
Kasra Jannati <kasra_ja1991@yahoo.com>
2020-03-05 02:49:46 -0500

    form

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -1,22 +1,67 @@
-import React, { useRef } from 'react'
+import React, { useRef,useState } from 'react'
 import './App.css'
 
 function App() {
-  const fileRef = useRef()
+  // const fileRef = useRef()
 
-  const upload = () => {
-    const data = new FormData();
-    data.append('file', fileRef.files[0]);
-    data.append('name', 'CA_CERTIFICATE');
+  // const upload = () => {
+  //   const data = new FormData();
+  //   data.append('file', fileRef.files[0]);
+  //   data.append('name', 'CA_CERTIFICATE');
   
-    axios.post('localhost:3000/', data)
-  }
+  //   axios.post('localhost:3000/', data)
+  // }
+
+  
+
+  const [id, setId] = useState('');
+  const [controller, setController] = useState('');
+  const [data, setData] = useState('');
+
+  const printValues = e => {
+    e.preventDefault();
+    console.log(id,controller,data);
+  };
 
   return (
-    <div className="App">
-      <input type="file" ref={fileRef} />
-      <button onClick={upload}>upload</button>
-    </div>
+
+    // <div className="App">
+    //   <input type="file" ref={fileRef} />
+    //   <button onClick={upload}>upload</button>
+    // </div>
+    <form onSubmit={printValues}>
+      <label>
+      id:
+        <input
+          value={id}
+          onChange={event => setId(event.target.value)}
+          name="id"
+          type="text"
+        />
+      </label>
+      <br />
+      <label>
+      controller:
+        <input
+          value={controller}
+          onChange={event => setController(event.target.value)}
+          name="controller"
+          type="text"
+        />
+      </label>
+      <label>
+      data:
+        <input
+          value={data}
+          onChange={event => setData(event.target.value)}
+          name="data"
+          type="text"
+        />
+      </label>
+      <br />
+      <button>Submit</button>
+    </form>
+
   )
 }
 
