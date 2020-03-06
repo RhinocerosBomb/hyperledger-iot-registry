ca7213b227302e400d4291487bfbe8724be081a4
Jason <rhinobomb@gmail.com>
2020-03-05 20:45:52 -0500

    Setup express server
    - Setup express server with utils to connect to the network

--- application/server/app.js
+++ application/server/app.js
@@ -4,7 +4,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
+var deviceRouter = require('./routes/device');
 
 var app = express();
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
+app.use('/device', deviceRouter);
 
 module.exports = app;
---
+++ application/server/routes/admin.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var fabricUtils = require('../utils/fabricUtils')
+var router = express.Router()
+
+/* GET device listing. */
+router.get('/', async function(req, res, next) {
+  try {
+    const network = fabricUtils.getNetwork('admin', 'connection-org1', 'myChannel');
+
+    // Get the contract from the network.
+    const contract = network.getContract('iotRegistry')
+
+    const result = await contract.evaluateTransaction('queryAllDevices')
+    console.log(
+      `Transaction has been evaluated, result is: ${result.toString()}`,
+    )
+    res.staus(200).json(JSON.parse(result.toString()))
+  } catch (error) {
+    console.error(`Failed to evaluate transaction: ${error}`)
+    res.status(500).json(error)
+  }
+})
+
+module.exports = router
